b0389870d17226f8d5ca0de0fce1be2e4f6d14f2
Gustavo Saavedra <tavito.286@gmail.com>
2013-01-13 08:07:30 -0430

    mejoras en el algoritmo de movimiento.

--- juego.js
+++ juego.js
@@ -1,8 +1,8 @@
 // CREAR OBJETO CANVAS
 var canvas = document.createElement("canvas");
 var ctx = canvas.getContext("2d");
-canvas.width = 437;
-canvas.height = 390;
+canvas.width = 850;
+canvas.height = 400;
 document.getElementById('contenedor').appendChild(canvas);
 
 //VARIABLES A CAMBIAR
@@ -11,10 +11,10 @@ var iniciar = false;
 var colision = false;
 
 //VARIABLES FIJAS
-var len 				 = 48;
+var len 				 = 50;
 var unidades_por_columna = 6;
-var unidades_por_fila  	 = 2;
-var crear_unidad_tipo	 = 0;
+var unidades_por_fila  	 = 8;
+var crear_unidad_tipo	 = 99;
 var rango				 = 48;
 
 // IMAGEN DE FONDO
@@ -47,6 +47,11 @@ var unidadSinImagen = new Image();
 unidadSinImagen.onload = function () { magoReady = true; };
 unidadSinImagen.src = "images/sinimagen.png";
 
+// IMAGEN - UNIDAD SIN IMAGEN (UNIDAD NULA)
+var castilloImagen = new Image();
+castilloImagen.onload = function () { magoReady = true; };
+castilloImagen.src = "images/castillo.png";
+
 // DEFINICIÓN DE UNIDADES
 function unidad_null(id){
 	this.id			= id || -1;
@@ -58,12 +63,22 @@ function unidad_null(id){
 	this.height		= 48;
 	this.parte		= 0;
 }
+function unidad_castillo(id){
+	this.id			= id || -1;
+	this.tipo		= 99;
+	this.rango		= 0;
+	this.velocidad	= -1;
+	this.colision	= true;
+	this.width		= 48;
+	this.height		= 48;
+	this.parte		= 0;
+}
 function unidad_soldado(id){
 	this.id			= id || -1;
 	this.tipo 		= 0;
 	this.rango 		= 0;
 	this.velocidad 	= 0.7;
-	this.colision	= false;
+	this.colision	= true;
 	this.width		= 48;
 	this.height		= 48;
 	this.parte		= 0;
@@ -73,7 +88,7 @@ function unidad_arquero(id){
 	this.tipo 		= 1;
 	this.rango 		= 3;
 	this.velocidad 	= 0.5;
-	this.colision	= false;
+	this.colision	= true;
 	this.width		= 48;
 	this.height		= 48;
 	this.parte		= 0;
@@ -83,7 +98,7 @@ function unidad_mago(id){
 	this.tipo 		= 2;
 	this.rango 		= 2;
 	this.velocidad 	= 0.4;
-	this.colision	= false;
+	this.colision	= true;
 	this.width		= 48;
 	this.height		= 48;
 	this.parte		= 0;
@@ -104,12 +119,21 @@ limpiar_campo();
 crear_retador(1);
 
 // LLENAR CAMPO DE BATALLA CON NULLS
+function crear_castillos(){
+	for(i=0;i<2;i++){
+
+	}
+}
 function limpiar_campo(){
 	for(i=0;i<unidades_por_columna;i++){
 		for(j=0;j<unidades_por_fila;j++){
-			equipo_jugador[i][j] = new unidad_null();
-			equipo_jugador[i][j].x = 51 + j*len; //51 porque 50 es el marco del tablero.
-			equipo_jugador[i][j].y = 51 + i*len;
+			if(j==0){
+				equipo_jugador[i][j] = new unidad_castillo();
+			}else{
+				equipo_jugador[i][j] = new unidad_null();
+			}
+			equipo_jugador[i][j].x = 2 + j*len; //52 porque 50 es el marco del tablero.
+			equipo_jugador[i][j].y = 52 + i*len;
 		}
 	}
 }
@@ -120,16 +144,23 @@ function iniciar_crear_unidad(){
 			unidad = equipo_jugador[i][j];
 			if(clickdX>unidad.x && clickdX<unidad.x+unidad.width){
 				if(clickdY>unidad.y && clickdY<unidad.y+unidad.height){
-					switch(crear_unidad_tipo){
-						case 0:
-						  equipo_jugador[i][j] = new unidad_soldado((i*7+j*11)+1);
-						  break;
-						case 1:
-						  equipo_jugador[i][j] = new unidad_arquero(i*7+j*11);
-						  break;
-						case 2:
-						  equipo_jugador[i][j] = new unidad_mago(i*7+j*11);
-						  break;
+					if(equipo_jugador[i][j].tipo<0){
+						switch(crear_unidad_tipo){
+							case 0:
+							  equipo_jugador[i][j] = new unidad_soldado((i*7+j*11)+1);
+							  break;
+							case 1:
+							  equipo_jugador[i][j] = new unidad_arquero(i*7+j*11);
+							  break;
+							case 2:
+							  equipo_jugador[i][j] = new unidad_mago(i*7+j*11);
+							  break;
+							case 99:
+							  equipo_jugador[i][j] = new unidad_castillo(i*7+j*11);
+							  break;
+						}
+					}else{
+
 					}
 					equipo_jugador[i][j].x = unidad.x;
 					equipo_jugador[i][j].y = unidad.y;
@@ -153,25 +184,29 @@ function crear_retador(nivel){
 		for(j=0;j<unidades_por_fila;j++){
 			equipo_retador[i][j] = new unidad_null();
 			equipo_retador[i][j].x = 381 + j*len; //51 porque 50 es el marco del tablero.
-			equipo_retador[i][j].y = 51 + i*len;
+			equipo_retador[i][j].y = 52 + i*len;
 		}
 	}
 	for(i=0;i<unidades_por_columna;i++){
 		for(j=0;j<unidades_por_fila;j++){
-			random = Math.floor(Math.random() * (max - min + 1)) + min;
-			switch(parseInt(random/10)){
-				case 0:
-					equipo_retador[i][j] = new unidad_soldado((i*7+j*11)+2);
-					break;
-				case 1:
-					equipo_retador[i][j] = new unidad_arquero((i*7+j*11)+2);
-					break;
-				case 2:
-					equipo_retador[i][j] = new unidad_mago((i*7+j*11)+2);
-					break;
+			if(j==unidades_por_fila-1){
+				equipo_retador[i][j] = new unidad_castillo();
+			}else{
+				random = Math.floor(Math.random() * (max - min + 1)) + min;
+				switch(parseInt(random/10)){
+					case 0:
+						equipo_retador[i][j] = new unidad_null((i*7+j*11)+2);
+						break;
+					case 1:
+						equipo_retador[i][j] = new unidad_null((i*7+j*11)+2);
+						break;
+					case 2:
+						equipo_retador[i][j] = new unidad_null((i*7+j*11)+2);
+						break;
+				}
 			}
-			equipo_retador[i][j].x = 290 + j*len; //51 porque 50 es el marco del tablero.
-			equipo_retador[i][j].y = 51 + i*len;
+			equipo_retador[i][j].x = 452 + j*len;
+			equipo_retador[i][j].y = 52 + i*len;
 		}
 	}
 }
@@ -180,16 +215,18 @@ function crear_retador(nivel){
 function determinar_frontline(){
 		// 1.1 INICIALIZAR EL FRONTLINE USANDO LA PRIMERA COLUMNA
 		for(i=0;i<unidades_por_columna;i++){
-			frontline_retador[i]=equipo_retador[i][0];
+			frontline_retador[i]=equipo_retador[i][unidades_por_fila-1];
 		}
 		for(i=0;i<unidades_por_columna;i++){
-			frontline_jugador[i]=equipo_jugador[i][unidades_por_fila-1];
+			frontline_jugador[i]=equipo_jugador[i][0];
 		}
-		// 1.2 DETERMINAR FRONTLINE MEDIANTE EL MENOR X DE LA FILA.
+		//1.2 DETERMINAR FRONTLINE MEDIANTE EL MENOR X DE LA FILA.
 		for(i=0;i<unidades_por_columna;i++){
 			for(j=0;j<unidades_por_fila;j++){
 				if (frontline_retador[i].x>equipo_retador[i][j].x){
-					frontline_retador[i]=equipo_retador[i][j];
+					if(equipo_retador[i][j].colision){
+						frontline_retador[i]=equipo_retador[i][j];
+					}
 				}
 			}
 		}
@@ -197,7 +234,9 @@ function determinar_frontline(){
 		for(i=0;i<unidades_por_columna;i++){
 			for(j=0;j<unidades_por_fila;j++){
 				if (frontline_jugador[i].x<equipo_jugador[i][j].x){
-					frontline_jugador[i]=equipo_jugador[i][j];
+					if(equipo_jugador[i][j].colision){
+						frontline_jugador[i]=equipo_jugador[i][j];
+					}
 				}
 			}
 		}
@@ -220,6 +259,7 @@ addEventListener("mouseup",function(e){
 // HACER UPDATE DE LOS OBJETOS
 var update = function (modifier) {
 	if (38 in keysDown) {
+		unidadSinImagen.src = "";
 		iniciar = true;
 	}
 	if(keysDown[-1]){
@@ -245,11 +285,23 @@ var update = function (modifier) {
 				for(y=0;y<unidades_por_fila;y++){ // RECORRER ELEMENTOS ENEMIGOS DE LA MISMA FILA
 					if (equipo_retador[i][y]==frontline_retador[i]) { // SI ELEMENTO ENEMIGO ES FRONTLINE
 						if(rango_unidad_jugador<equipo_retador[i][y].x){	// SI UNIDAD ESTA EN RANGO CON FRONTLINE 
-							if(j==unidades_por_fila-1){			// SI ES FRONTLINE DEL EJERCITO PROPIO (TO.DO)
-								equipo_jugador[i][j].x += 1 * equipo_jugador[i][j].velocidad;	// AVANZAR LO MÁS POSIBLE
+							if(j==unidades_por_fila-1){
+								if(equipo_jugador[i][j].velocidad>0){
+									equipo_jugador[i][j].x += 1 * equipo_jugador[i][j].velocidad;	// AVANZAR LO MÁS POSIBLE
+								}
 							}else{ // SI NO ES FRONTLINE DEL EJERCITO PROPIO (2DA COLUMNA)
-								if(equipo_jugador[i][j].x + equipo_jugador[i][j].width < equipo_jugador[i][j+1].x){
-									equipo_jugador[i][j].x += 1 * equipo_jugador[i][j].velocidad;
+								for(k=j+1;k<unidades_por_fila;k++){
+									if(equipo_jugador[i][k].colision){
+										if(equipo_jugador[i][j].x + equipo_jugador[i][j].width < equipo_jugador[i][k].x){
+											if(equipo_jugador[i][j].velocidad>0){
+												equipo_jugador[i][j].x += 1 * equipo_jugador[i][j].velocidad;	// AVANZAR LO MÁS POSIBLE
+											}
+										}
+										break;	
+									}else{
+										equipo_jugador[i][k] = equipo_jugador[i][j];
+										equipo_jugador[i][j] = new unidad_null();
+									}
 								}
 							}
 						}else{
@@ -259,10 +311,18 @@ var update = function (modifier) {
 					if (equipo_jugador[i][y]==frontline_jugador[i]) {
 						if(rango_unidad_retador>equipo_jugador[i][y].x){
 							if(j==0){	
-								equipo_retador[i][j].x -= 1 * equipo_retador[i][j].velocidad;
-							}else{
-								if(equipo_retador[i][j].x > equipo_retador[i][j-1].x + equipo_retador[i][j-1].width){
+								if(equipo_retador[i][j].velocidad>0){
 									equipo_retador[i][j].x -= 1 * equipo_retador[i][j].velocidad;
+								}
+							}else{
+								for(k=j-1;k<unidades_por_fila;k++){
+									if(equipo_retador[i][k].colision){
+										if(equipo_retador[i][j].x > equipo_retador[i][j-1].x + equipo_retador[i][j-1].width){
+											if(equipo_retador[i][j].velocidad>0){
+												equipo_retador[i][j].x -= 1 * equipo_retador[i][j].velocidad;
+											}
+										}
+									}
 								}
 							}
 						}else{
@@ -297,6 +357,9 @@ var render = function () {
 					case 2:
 					  ctx.drawImage(magoImagen, equipo_jugador[i][j].x, equipo_jugador[i][j].y);
 					  break;
+					case 99:
+					  ctx.drawImage(castilloImagen, equipo_jugador[i][j].x, equipo_jugador[i][j].y);
+					  break;
 				}
 				switch(equipo_retador[i][j].tipo){
 					case -1:
@@ -310,6 +373,9 @@ var render = function () {
 					  break;
 					case 2:
 					  ctx.drawImage(magoImagen, equipo_retador[i][j].x, equipo_retador[i][j].y);
+					  break;
+					case 99:
+					  ctx.drawImage(castilloImagen, equipo_retador[i][j].x, equipo_retador[i][j].y);
 					  break;
 				}
 			}
